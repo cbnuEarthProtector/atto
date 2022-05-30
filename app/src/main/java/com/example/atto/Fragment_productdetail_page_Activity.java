8832c9ef797d7c259cafdccb68c7f617852bd81b
sarah2234 <2234sarah@gmail.com>
2022-05-31 00:35:54 +0900

    feat: connect Internet

--- app/src/main/java/com/example/atto/Fragment_productdetail_page_Activity.java
+++ app/src/main/java/com/example/atto/Fragment_productdetail_page_Activity.java
@@ -1,6 +1,9 @@
 package com.example.atto;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -13,11 +16,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.atto.database.AppDatabase;
 import com.example.atto.database.Product;
 import com.example.atto.database.ProductDao;
 import com.example.atto.database.ProductWithBrandName;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class Fragment_productdetail_page_Activity extends Fragment {
@@ -25,7 +32,8 @@ public class Fragment_productdetail_page_Activity extends Fragment {
     Button scrapBtn;
 
     private List<ProductWithBrandName> productList;
-    public Fragment_productdetail_page_Activity(){
+
+    public Fragment_productdetail_page_Activity() {
         //productList = new Product();
     }
 
@@ -38,6 +46,14 @@ public class Fragment_productdetail_page_Activity extends Fragment {
         ProductDao productDao = appDatabase.productDao();
         productList = productDao.getAll(); ////
         scrapBtn = fv.findViewById(R.id.scrapBtn);
+
+        // Fragment에서는 Activity를 호출하지 못하므로 부모 Activity를 이용하여 호출
+        Intent intent = new Intent(getActivity(), Fragment_marcket_page_Activity.class);
+        // int productId = intent.getIntExtra("id", 0);
+
+        int productId = 0;
+        //Glide.with(getActivity()).load(productList.get(0).photoURL).into(productImage);
+        // 이미지를 url로 설정: https://lakue.tistory.com/35
 
         scrapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
