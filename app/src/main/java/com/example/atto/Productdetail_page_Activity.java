2863a8496d1e351449255ef82af22873f0cc1abd
sarah2234 <2234sarah@gmail.com>
2022-06-03 22:28:43 +0900

    fix: 상품 상세페이지 출력 시 강제 종료 오류 수정

--- app/src/main/java/com/example/atto/Productdetail_page_Activity.java
+++ app/src/main/java/com/example/atto/Productdetail_page_Activity.java
@@ -3,14 +3,10 @@ package com.example.atto;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.*;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.atto.database.AppDatabase;
@@ -18,7 +14,7 @@ import com.example.atto.database.ProductDao;
 import com.example.atto.database.ProductWithBrandName;
 import java.util.List;
 
-public class Fragment_productdetail_page_Activity extends Fragment {
+public class Productdetail_page_Activity extends AppCompatActivity {
     ImageView productImage;
     ImageButton scrapBtn;
     Button productPageBtn;
@@ -26,32 +22,34 @@ public class Fragment_productdetail_page_Activity extends Fragment {
 
     private List<ProductWithBrandName> productList;
 
-    public Fragment_productdetail_page_Activity() {
+    public Productdetail_page_Activity() {
         //productList = new Product();
     }
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View fv = inflater.inflate(R.layout.activity_fragment_productdetail_page, container, false);
-        productImage = fv.findViewById(R.id.productImageView);
-        AppDatabase appDatabase = AppDatabase.getInstance(getActivity().getApplicationContext());
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productdetail_page);
+
+        productImage = findViewById(R.id.productImageView);
+        AppDatabase appDatabase = AppDatabase.getInstance(getApplicationContext());
         ProductDao productDao = appDatabase.productDao();
         productList = productDao.getAll();
-        scrapBtn = fv.findViewById(R.id.scrapBtn);
-        productPageBtn = fv.findViewById(R.id.productPageBtn);
-        brandField = fv.findViewById(R.id.brandField);
-        productNameField = fv.findViewById(R.id.productNameField);
-        priceField = fv.findViewById(R.id.priceField);
+        scrapBtn = findViewById(R.id.scrapBtn);
+        productPageBtn = findViewById(R.id.productPageBtn);
+        brandField = findViewById(R.id.brandField);
+        productNameField = findViewById(R.id.productNameField);
+        priceField = findViewById(R.id.priceField);
 
-        int productId = getArguments().getInt("id");
+        Intent intent = getIntent();
+        int productId = intent.getIntExtra("id", 0);
         ProductWithBrandName matchingProduct = productList.get(productId - 1); // 배열 인덱스로 상품 찾기
         if(productId != matchingProduct.id) { // 배열 인덱스로 찾았을 때 id 값이 일치하지 않을 때
             for (ProductWithBrandName product : productList) {
                 if (product.id == productId) matchingProduct = product; // Fragment_market_page_Activity에서 전달받은 id 값과 일치하는 상품 찾기
             }
         }
-        Glide.with(getActivity()).load(matchingProduct.photoURL).into(productImage);
+        Glide.with(this).load(matchingProduct.photoURL).into(productImage);
         brandField.setText(matchingProduct.brandName); // 브랜드명
         productNameField.setText(matchingProduct.name); // 상품명
         priceField.setText(Integer.toString(matchingProduct.price) + "원"); // 가격
@@ -60,12 +58,12 @@ public class Fragment_productdetail_page_Activity extends Fragment {
         scrapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), MypageScrapActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MypageScrapActivity.class);
                 //intent.putExtra() // 스크랩 페이지로 전달해줄 값 설정
                 startActivity(intent);
             }
         });
-        
+
         // 상품 페이지로 연결
         productPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +72,5 @@ public class Fragment_productdetail_page_Activity extends Fragment {
                 startActivity(intent);
             }
         });
-
-        return fv;
     }
 }
